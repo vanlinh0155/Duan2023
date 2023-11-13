fa7a633d5b1aae077d691aeaaf5837293019f03e
anhquan2964 <139560577+anhquan2964@users.noreply.github.com>
2023-11-13 13:21:03 +0700

    text

--- admin/index.php
+++ admin/index.php
@@ -99,6 +99,13 @@ if(isset($_GET['act'])){
                     $listdanhmuc = loadall_danhmuc(); 
                     include "sanpham/update.php";
                     break;
+            case 'sua';
+                if(isset($_GET['id'])&&($_GET['id']>0)){
+                    $sanpham=loadone_sanpham($_GET['id']);
+                }
+                    $listdanhmuc = loadall_danhmuc(); 
+                    include "sanpham/update.php";
+                    break;
 
             case 'updatesp': 
                 if(isset($_POST['capnhat']) && ($_POST['capnhat'])){ 
